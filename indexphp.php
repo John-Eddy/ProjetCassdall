f6eb81163050336c1d0281420e2f7876c9b31fd1
rahmanieddy@gmail.com <rahamnieddy@gmail.com>
2016-01-19 14:20:32 +0100

    First commit

---
+++ indexphp.php
@@ -0,0 +1,222 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" >
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>CASSDALL CHAMPIER</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+
+    <!-- Custom Fonts -->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css" type="text/css">
+
+    <!-- Plugin CSS -->
+    <link rel="stylesheet" href="css/animate.min.css" type="text/css">
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="css/creative.css" type="text/css">
+    <link rel="stylesheet" href="css/mtree.css" type="text/css">
+
+
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body id="page-top" onload="onLoad();">
+
+<nav id="mainNav" class="navbar navbar-default navbar-fixed-top">
+    <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand page-scroll red" href="#page-top">
+                <img src="img/logo.svg" alt="logo" class="img-responsive" id="logo-brand"/>
+                CASSDALL CHAMPIER
+            </a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right">
+                <li>
+                    <a class="page-scroll" href="#telephone">Telephone</a>
+                </li>
+                <li>
+                    <a class="page-scroll" href="#menu">Menu</a>
+                </li>
+                <li>
+                    <a class="page-scroll" href="#contact">Nous trouver</a>
+                </li>
+            </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container-fluid -->
+</nav>
+
+
+<section class="bg-primary" id="telephone" ">
+
+<div class="container">
+    <div class="row">
+        <div class="col-lg-8 col-lg-offset-2 text-center header-text">
+            <img src="img/logo.svg" alt="logo" class="img-responsive"/>
+            <hr class="light">
+            <h2 class=" text-faded">
+                <i class="  fa fa-phone fa-2x wow bounceIn"></i>
+                </br>
+                <strong>04 74 53 43 99</strong>
+            </h2>
+            <a href="#menu" class="btn btn-default btn-xl btn-green page-scroll">Menu</a>
+        </div>
+    </div>
+</div>
+</section>
+
+<section id="menu">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <h2 class="section-heading">Menu</h2>
+                <hr class="primary">
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <!-- This is mtree list -->
+                    <ul class="mtree">
+                        <li><h4 class="li-title text-center">Nos Sandwichs Tacos</h4>
+                            <ul>
+                                <li>
+                                    <div class="col-lg-3 col-sm-4 col-xs-6 plat text-center text-faded">
+                                        <img src="img/Menu/tacos/tacos-steak.png" alt="" class="img-plat"/>
+                                        <h4 class="nom-plat ">TACOS</h4>
+                                        <p >
+                                            5 € seul / 7,50 € en menu
+                                        </p>
+                                    </div>
+                                </li>
+
+                            </ul>
+                        </li>
+                        <li><h4 class="li-title text-center">Nos Burgers</h4>
+                            <ul>
+                                <li><a href="#">China</a></li>
+                                <li><a href="#">India</a></li>
+                                <li><a href="#">Malaysia</a></li>
+                                <li><a href="#">Thailand</a></li>
+                                <li><a href="#">Vietnam</a></li>
+                                <li><a href="#">Singapore</a></li>
+                                <li><a href="#">Indonesia</a></li>
+                                <li><a href="#">Mongolia</a></li>
+                            </ul>
+
+                        </li>
+                        <li><h4 class="li-title text-center">Nos Assiettes</h4>
+                            <ul>
+                                <li><a href="#">China</a></li>
+                                <li><a href="#">India</a></li>
+                                <li><a href="#">Malaysia</a></li>
+                                <li><a href="#">Thailand</a></li>
+                                <li><a href="#">Vietnam</a></li>
+                                <li><a href="#">Singapore</a></li>
+                                <li><a href="#">Indonesia</a></li>
+                                <li><a href="#">Mongolia</a></li>
+                            </ul>
+                        </li>
+
+                        </li>
+                        <li><h4 class="li-title text-center">Nos Assiettes</h4>
+                            <ul>
+                                <li><a href="#">China</a></li>
+                                <li><a href="#">India</a></li>
+                                <li><a href="#">Malaysia</a></li>
+                                <li><a href="#">Thailand</a></li>
+                                <li><a href="#">Vietnam</a></li>
+                                <li><a href="#">Singapore</a></li>
+                                <li><a href="#">Indonesia</a></li>
+                                <li><a href="#">Mongolia</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+        </div>
+    </div>
+</section>
+
+<aside class="bg-dark">
+    <div class="container text-center">
+        <div class="call-to-action">
+            <h2>Free Download at Start Bootstrap!</h2>
+            <a href="#" class="btn btn-default btn-xl wow tada">Download Now!</a>
+        </div>
+    </div>
+</aside>
+
+<section id="contact">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2 text-center">
+                <h2 class="section-heading">Nous Trouver</h2>
+                <hr class="primary">
+            </div>
+            <div class="col-xs-12">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2799.017241374493!2d5.287864115556972!3d45.44930867910079!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x478ad7ad42c6a2b3%3A0x3408772566949177!2sLe+Cass&#39;dall+Champier!5e0!3m2!1sfr!2sfr!4v1452691570104" width="100%" height="600" frameborder="0" style="border:0; margin: 50px 0px 50px 0px;" allowfullscreen></iframe>
+            </div>
+            <div class="col-lg-4 col-lg-offset-2 text-center ">
+                <i class="fa fa-phone fa-3x wow bounceIn"></i>
+                <hr class="primary">
+                <p  class=>04 74 53 43 99 </p>
+                </div>
+
+            <div class="col-lg-4 text-center">
+                <i class="fa fa-clock-o fa-3x wow bounceIn" data-wow-delay=".1s"></i>
+                <hr class="primary">
+                <p>
+                    Du mardi au dimanche
+                    </br> 11h - 14h / 18h - 21h
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- jQuery -->
+<script src="js/jquery.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="js/bootstrap.min.js"></script>
+
+<!-- Plugin JavaScript -->
+<script src="js/jquery.easing.min.js"></script>
+<script src="js/jquery.fittext.js"></script>
+<script src="js/wow.min.js"></script>
+
+<!-- Custom Theme JavaScript -->
+<script src="js/creative.js"></script>
+<script src="js/mtree.js"></script>
+
+</body>
+
+</html>
